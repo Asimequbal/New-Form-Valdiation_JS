d0e218518be26cecd9b51d57337c399a9f4311e6
Asim Equbal <114874702+Asimequbal@users.noreply.github.com>
2022-11-27 00:59:08 +0530

    css finished

---
+++ script.js
@@ -0,0 +1,12 @@
+function valdidateForm(){
+let FullName = document.querySelector('#fullName').vlaue
+let fullNameLength = FullName.length
+
+
+
+if (userNameLength <=5){
+    document.querySelector('.error_message').innerText = 'Atleast 5 Character required'
+}else{
+    document.querySelector('')
+}
+}
